341187adc510c807f946e303bc089885b8a4a661
DemendiAkos <akosdemendi@gmail.com>
2024-09-20 14:49:01 +0200

    did some tasks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { quotes } from './quotes';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -12,4 +14,44 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+  @Get('quotes')
+  @Render('quotespage')
+  getQuotes() {
+    return {
+      quotes: quotes.quotes,
+    }
+  }
+
+  @Get('random')
+  @Render('randomquote')
+  getRandomQuote() {
+    let random = Math.floor(Math.random() * quotes.quotes.length);
+    return {
+      quote: quotes.quotes[random],
+    }
+  }
+
+  @Get("top")
+  @Render("topauthors")
+  getTopAuthors() {
+    const authors = quotes.quotes.map(quote => quote.author);
+    const dict: { [key: string]: number } = {};
+    authors.forEach(author => {
+      if(dict[author]) {
+        dict[author] += 1;
+      } else {
+        dict[author] = 1;
+      }
+    });
+   
+    const sortedAuthorDict = Object.entries(dict)
+    .sort(([, a], [, b]) => b - a)
+    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+  return { 
+    authorDict: sortedAuthorDict
+  };
+  }
 }
+  
+
