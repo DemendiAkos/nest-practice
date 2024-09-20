0272bd658642313a06d29912608a86999aa418ce
DemendiAkos <akosdemendi@gmail.com>
2024-09-20 16:08:52 +0200

    feat: added get qoute by Id and delete qoute by id

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { quotes } from './quotes';
 
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  @Render('index')
+  @Render('quotespage')
   getHello() {
     return {
       message: this.appService.getHello()
@@ -38,20 +38,49 @@ export class AppController {
     const authors = quotes.quotes.map(quote => quote.author);
     const dict: { [key: string]: number } = {};
     authors.forEach(author => {
-      if(dict[author]) {
+      if (dict[author]) {
         dict[author] += 1;
       } else {
         dict[author] = 1;
       }
     });
-   
+
     const sortedAuthorDict = Object.entries(dict)
-    .sort(([, a], [, b]) => b - a)
-    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
-  return { 
-    authorDict: sortedAuthorDict
-  };
+      .sort(([, a], [, b]) => b - a)
+      .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+    return {
+      authorDict: sortedAuthorDict
+    };
+  }
+
+
+  @Get('quotes/:id')
+  @Render('onequotetorulethemall')
+  oneQuote(@Param('id') id: string) {
+    console.log(quotes.quotes[id]);
+    return {
+      quote: quotes.quotes[id]
+    }
+  }
+
+  @Get('delete/:id')
+  @Render('deleted')
+  deleteSingleQuote(@Param('id') id: string) {
+    const itemIndex = quotes.quotes.findIndex((quote) => quote.id === parseInt(id)); 
+    if (itemIndex === -1) {
+      return { 
+        delete: false, 
+        text: `Quote with id ${id} not found.` 
+      };
+    }
+  
+    const deletedQuote = quotes.quotes.splice(itemIndex, 1)[0]; 
+    
+    return {
+      delete: true,
+      text: `Quote "${deletedQuote.quote}" has been successfully deleted.`
+    };
   }
 }
-  
+
 
