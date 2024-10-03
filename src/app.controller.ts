38332ae1b59863c28a1df12b57d968e7dcac4415
DemendiAkos <akosdemendi@gmail.com>
2024-10-03 12:58:53 +0200

    feat: params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Render } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Query, Redirect, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import { quotes } from './quotes';
 
@@ -66,21 +66,75 @@ export class AppController {
   @Get('delete/:id')
   @Render('deleted')
   deleteSingleQuote(@Param('id') id: string) {
-    const itemIndex = quotes.quotes.findIndex((quote) => quote.id === parseInt(id)); 
+    const itemIndex = quotes.quotes.findIndex((quote) => quote.id === parseInt(id));
     if (itemIndex === -1) {
-      return { 
-        delete: false, 
-        text: `Quote with id ${id} not found.` 
+      return {
+        delete: false,
+        text: `Quote with id ${id} not found.`
       };
     }
-  
-    const deletedQuote = quotes.quotes.splice(itemIndex, 1)[0]; 
-    
+
+    const deletedQuote = quotes.quotes.splice(itemIndex, 1)[0];
+
     return {
       delete: true,
       text: `Quote "${deletedQuote.quote}" has been successfully deleted.`
     };
   }
+
+  @Get('search')
+  @Render('searchquote')
+  searchQuote(@Query('input') input: string = '') {
+    const searchResult = quotes.quotes.filter((quote) => quote.quote.toLowerCase().includes(input.toLowerCase()));
+    return {
+      searchResult: searchResult
+    }
+  }
+
+  @Get('authorForm')
+  @Render('authorRandomForm')
+  authorForm(@Query('author') author: string = '') {
+    return {
+      url: `/authorRandom?author=${author}`
+    };
+  }
+
+  @Get('authorRandom')
+  @Render('authorRandom') 
+  authorRandom(@Query('author') author: string = '') {
+    const authorQuotes = quotes.quotes.filter((quote) =>
+      quote.author.toLowerCase().includes(author.toLowerCase())
+    );
+    if (authorQuotes.length === 0) {
+      return {
+        result: `No quotes found by author ${author}.`
+      };
+    } else {
+      console.log(authorQuotes[Math.floor(Math.random() * authorQuotes.length)])
+      return {
+        result: authorQuotes[Math.floor(Math.random() * authorQuotes.length)]
+      };
+    }
+  }
+
+  @Get('referenceform')
+  @Render('referenceform')
+  referenceForm(@Query('ref') ref: string = '') {
+    return {
+      url: `/displayref?ref=${ref}`
+    }
+  }
+
+  @Get('displayref')
+  @Render('displayref')
+  Findref(@Query('ref') ref: string = '') {
+    const refQuotes = quotes.quotes.filter((quote) =>
+      quote.quote.toLowerCase().includes(ref.toLowerCase())
+    );
+    return {
+      refQuotes: refQuotes
+    }
+
+  }
+
 }
-
-
